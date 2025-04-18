b96739d8fee646fc685e75862562cc0593cca22c
Asfandyar Ali <imasfandyarali@gmail.com>
2025-04-18 14:14:09 +0500

    services page minor update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -14,14 +14,14 @@ const services = [
   },
   {
     num: "02",
-    title: "UI/UX Design", // Fixed "U1" to likely intended "UI/UX Design"
+    title: "UI/UX Design",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.",
     href: "",
   },
   {
     num: "03",
-    title: "Creative Technology", // Changed duplicate "Web Development" to more relevant title
+    title: "Creative Technology",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.",
     href: "",
@@ -29,7 +29,7 @@ const services = [
 
   {
     num: "04",
-    title: "Creative Technology", // Changed duplicate "Web Development" to more relevant title
+    title: "Creative Technology",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.",
     href: "",
@@ -81,42 +81,3 @@ const ServicesPage = () => {
 };
 
 export default ServicesPage;
-
-// <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-//   <div className="container mx-auto">
-//     <motion.div
-//       initial={{ opacity: 0 }}
-//       animate={{
-//         opacity: 1,
-//         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-//       }}
-//       className="grid grid-cols-1 md:grid-cols-2 gap-[60vh]"
-//     >
-//       {services.map((service, index) => {
-//         return (
-//           <div
-//             key={index}
-//             className="flex-1 flex flex-col justifiy-center gap-6 group"
-//           >
-//             <div className="w-full flex justify-between items-center">
-//               <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
-//                 {service.num}
-//               </div>
-//               <Link
-//                 href={service.href}
-//                 className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
-//               >
-//                 <BsArrowDownRight className="text-primary text-3xl" />
-//               </Link>
-//             </div>
-//             <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
-//               {service.title}
-//             </h2>
-//             <h2 className="text-white/60">{service.description}</h2>
-//             <div className="border-b border-white/20 w-full"></div>
-//           </div>
-//         );
-//       })}
-//     </motion.div>
-//   </div>
-// </section>
