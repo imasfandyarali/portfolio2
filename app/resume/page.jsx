1a42d377234058e49a0aef185395b960603a33a6
Asfandyar Ali <imasfandyarali@gmail.com>
2025-04-19 01:00:58 +0500

    resume page started

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,51 @@
+"use client";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaPython,
+  FaWordpress,
+  FaAngular,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiFastapi,
+  SiAdobeaftereffects,
+  SiAdobeillustrator,
+  SiAdobeaudition,
+  SiAdobeindesign,
+  SiAdobecreativecloud,
+  SiAdobepremierepro,
+  SiAdobexd,
+  SiFigma,
+  SiBootstrap,
+} from "react-icons/si";
+
+//about Data
+const about = {
+  title: "John Doe",
+  description: "Full Stack Developer",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Asfandyar Ali",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Asfandyar Ali",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Asfandyar Ali",
+    },
+    {
+      fieldName: "Name",
+      fieldValue: "Asfandyar Ali",
+    },
+  ],
+};
 const Resume = () => {
   return <div>resume page</div>;
 };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 const links = [
   {
     name: "home",
-    path: "/contact",
+    path: "/",
   },
   {
     name: "services",
